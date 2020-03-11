ddb68621b9896cacd4d1f6fe624caaabe385db47
Luis PM <luisp237@gmail.com>
2020-03-11 13:57:52 -0400

    end of lecture on middleware in express

--- server.js
+++ server.js
@@ -10,13 +10,16 @@ const server = express();
 
 //global middleware
 //3rd party middleware
-// server.use(morgan("dev")); 
+server.use(morgan("dev")); 
 server.use(helmet()); //3
+
 server.use(express.json());  // built-in middleware: no need to npm install 
+server.use(logger); //dont envoke because is a VOID function and doesn't have a return statement
 
 server.use('/api/hubs', hubsRouter);
 
-server.get('/', (req, res) => {
+// server.use(name); is globally available but we only need it for one route so we will add it to params
+server.get('/',name, (req, res) => {
   const nameInsert = (req.name) ? ` ${req.name}` : '';
 
   res.send(`
@@ -25,11 +28,29 @@ server.get('/', (req, res) => {
     `);
 });
 
+
+server.use(function (req, res, next) {
+  res 
+    .status(404)
+    .json({message: "Oooooooooops, did not find what you're looking for"})
+})
+
 //the three amigas
 function logger (req, res, next){
   //log information about the request to the console -> GET to /
   const method = req.method;
   const endpoint = req.originalUrl;
   console.log(`${method} to ${endpoint}`);
+
+  next(); //moves the request to the next middleware
 }
+
+//name
+function name (req, res, next){
+  const newName = req.name ? req.name : 'John Doe';
+  req.name = newName;
+  next();
+}
+
+
 module.exports = server;
