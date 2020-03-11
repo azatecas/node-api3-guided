17172279581bdc22d0a5341fc894385eb7bb18c4
Luis PM <luisp237@gmail.com>
2020-03-11 13:03:56 -0400

    lecture guide commit

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require('express'); // importing a CommonJS module
+const morgan = require('morgan');
+const helmet = require('helmet');
 
 const hubsRouter = require('./hubs/hubs-router.js');
 
 const server = express();
 
-server.use(express.json());
+
+
+//global middleware
+//3rd party middleware
+// server.use(morgan("dev")); 
+server.use(helmet()); //3
+server.use(express.json());  // built-in middleware: no need to npm install 
 
 server.use('/api/hubs', hubsRouter);
 
@@ -13,8 +21,15 @@ server.get('/', (req, res) => {
 
   res.send(`
     <h2>Lambda Hubs API</h2>
-    <p>Welcome${nameInsert} to the Lambda Hubs API</p>
+    <p>Welcome: ${nameInsert}, to the Lambda Hubs API</p>
     `);
 });
 
+//the three amigas
+function logger (req, res, next){
+  //log information about the request to the console -> GET to /
+  const method = req.method;
+  const endpoint = req.originalUrl;
+  console.log(`${method} to ${endpoint}`);
+}
 module.exports = server;
